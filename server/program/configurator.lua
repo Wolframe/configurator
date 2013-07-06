1d81b1d81ad8f314184b804572ee437a980c7a31
Andreas Baumann <abaumann@yahoo.com>
2013-07-06 10:58:37 +0200

    started to remove superfluos Lua code (configurator form plugin for now)

--- server/program/configurator.lua
+++ server/program/configurator.lua
@@ -644,34 +644,3 @@ end
 function deletePicture( )
 	delete_picture( input:get())
 end
-
--- configurator (the C++ plugin)
-
-function ConfiguredComponentsFixRequest( )
-	local t = formfunction( "selectConfiguredComponentsFix" )( { configID = input:table( )["component"]["configID"] } )
-	local f = form( "ConfiguredComponentsFix" )
-	f:fill( t:get( ) )
-	output:print( f:get( ) )
-end
-
-function ConfiguredComponentsUserRequest( )
-	local t = formfunction( "selectConfiguredComponentsUser" )( { configID = input:table( )["component"]["configID"] } )
-	local f = form( "ConfiguredComponentsUser" )
-	f:fill( t:get( ) )
-	output:print( f:get( ) )
-end
-
-function ConfiguratorRequiredFeaturesRequest( )
-	local t = formfunction( "selectRequiredFeatures" )( { configID = input:table( )["configuration"]["configID"] } )
-	local f = form( "ConfiguratorRequiredFeatures" )
-	f:fill( t:get( ) )
-	output:print( f:get( ) )
-end
-
-function ConfiguratorAddComponentRequest( )
-	local t = formfunction( "ConfiguratorAddComponent" )( input:table( ) )
-end
-
-function ConfiguratorDeleteComponentRequest( )
-	local t = formfunction( "ConfiguratorDeleteComponent" )( input:table( ) )
-end
