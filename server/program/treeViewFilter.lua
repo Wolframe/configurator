d6aaa110f50a5adeb0d02c8893421c8c98ac331d
patrickfrey <patrickpfrey@yahoo.com>
2013-09-20 16:24:57 +0200

    tree view filter

---
+++ server/program/treeViewFilter.lua
@@ -0,0 +1,57 @@
+
+local function map_tree_node( treenodes, nodeid, tagname)
+	local tree = nil
+	if treenodes[ nodeid ] then
+		tree[ "id"] = nodeid
+		tree[ tagname] = tree[ nodeid ].name
+		if tree[ nodeid ].description then
+			tree[ "description"] = tree[ nodeid ].description
+		end
+		local children = {}
+		for i,v in pairs( tree[ nodeid].children) do
+			local childnode = map_tree_node( treenodes, v, tagname)
+			table.insert( children, childnode)
+		end
+		if #children > 0 then
+			tree[ "item"] = children
+		end
+	end
+	return tree
+end
+
+-- IN tree := { name = treename, nodes = { name=STRING, description=STRING, pictures=INT[], parentID=INT, children=INT[] } }
+function treefilter( tree)
+	local id2nodemap = {}
+	local parentmap = {}
+	local tagname = tree[ "name"]
+	local treenodes = tree[ "nodes"]
+
+	for i,v in pairs( treenodes) do
+		local pi = tonumber( v.parentID)
+		local ti = tonumber( v.ID)
+		parentmap[ ti] = pi
+		table.insert( id2nodemap, ti, { name=v.name, description=v.description, pictures=v.pictures, parentID=pi, children = {} } )
+	end
+	local rootID = nil
+	for i,v in pairs( parentmap) do
+		if not parentmap[ v] then
+			if rootID then
+				error("filter tree called with multiple root nodes")
+			end
+			rootID = i
+		end
+	end
+	if rootID then
+		for i,v in pairs( id2nodemap) do
+			if i ~= rootID and v.parentID then
+				table.insert( id2nodemap[ v.parentID ].children, i )
+			end
+		end
+		local rt = {}
+		rt[ "item"] = map_tree_node( id2nodemap, rootID, tagname)
+		return rt
+	else
+		return nil
+	end
+end
+
