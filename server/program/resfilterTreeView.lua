4a7982cc4dbebe88c1fac45588279dc4cef2ecbf
patrickfrey <patrickpfrey@yahoo.com>
2013-09-22 14:36:35 +0200

    continue rewrite -- work in progress

--- server/program/resfilterTreeView.lua
+++ server/program/resfilterTreeView.lua
@@ -1,9 +1,8 @@
 
-local function map_tree_node( treenodes, nodeid, tagname)
+local function map_tree_node( treenodes, nodeid)
 	local tree = nil
 	if treenodes[ nodeid ] then
-		tree[ "id"] = nodeid
-		tree[ tagname] = treenodes[ nodeid ].name
+		tree = { id = nodeid, name = treenodes[ nodeid ].name }
 		if treenodes[ nodeid ].description then
 			tree[ "description"] = treenodes[ nodeid ].description
 		end
@@ -12,7 +11,7 @@ local function map_tree_node( treenodes, nodeid, tagname)
 		end
 		local children = {}
 		for i,v in pairs( treenodes[ nodeid].children) do
-			local childnode = map_tree_node( treenodes, v, tagname)
+			local childnode = map_tree_node( treenodes, v)
 			table.insert( children, childnode)
 		end
 		if #children > 0 then
@@ -25,20 +24,26 @@ end
 -- IN tree := { name = treename, node = { name=STRING, description=STRING, pictures=INT[], parentID=INT, children=INT[] } }
 function resfilterTreeView( tree_)
 	local tree = tree_:table()
+	logger.printc( "TREE=", tree)
 	local id2nodemap = {}
 	local parentmap = {}
 	local tagname = tree[ "name"]
 	local treenodes = tree[ "node"]
 
+	logger.printc( "TREENODES=", treenodes)
 	for i,v in pairs( treenodes) do
-		local pi = tonumber( v.parentID)
+		local pi = 0
+		if v.parentID then
+			pi = tonumber( v.parentID)
+		end
 		local ti = tonumber( v.ID)
 		parentmap[ ti] = pi
 		table.insert( id2nodemap, ti, { name=v.name, description=v.description, pictures=v.pictures, parentID=pi, children = {} } )
 	end
+	logger.printc( "PARENTMAP=", parentmap)
 	local rootID = nil
 	for i,v in pairs( parentmap) do
-		if not parentmap[ v] then
+		if not parentmap[ v] or parentmap[ v] == 0 then
 			if rootID then
 				error("filter tree called with multiple root nodes")
 			end
@@ -52,9 +57,11 @@ function resfilterTreeView( tree_)
 			end
 		end
 		local rt = {}
-		rt[ "item"] = map_tree_node( id2nodemap, rootID, tagname)
+		rt[ "item"] = map_tree_node( id2nodemap, rootID)
+		logger.printc( "RESULT resfilterTreeView: ", rt)
 		return rt
 	else
+		logger.printc( "RESULT OF resfilterTreeView IS EMPTY")
 		return nil
 	end
 end
