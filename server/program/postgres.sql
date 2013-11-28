236d3ec09dda1818e946a8e06eeee71bb05873ef
patrickfrey <patrickpfrey@yahoo.com>
2013-11-28 23:13:49 +0100

    replace DB programs with proprietary PREPARE syntax by database specific SUBROUTINEs in TDLs

--- server/program/postgres.sql
+++
@@ -1,9 +0,0 @@
-PREPARE getLastPictureID AS SELECT DISTINCT currval( pg_get_serial_sequence( 'picture', 'id' ) );
-PREPARE getLastConfigurationID AS SELECT DISTINCT currval( pg_get_serial_sequence( 'configuration', 'id' ) ) AS "id";
-PREPARE createIdList AS CREATE LOCAL TEMPORARY TABLE IDlist (ID INT) ON COMMIT DROP;
-PREPARE createIdUnion AS CREATE LOCAL TEMPORARY TABLE IDunion (ID INT) ON COMMIT DROP;
-PREPARE dropIdList AS DROP TABLE IDList;
-PREPARE dropIdUnion AS DROP TABLE IDUnion;
-PREPARE createFeatureIDlist AS CREATE LOCAL TEMPORARY TABLE IDlist (featureID INT, minQuantity INT, maxQuantity INT) ON COMMIT DROP;
-PREPARE createComponentIDlist AS CREATE LOCAL TEMPORARY TABLE IDlist (componentID INT, quantity INT) ON COMMIT DROP;
-PREPARE createCategoryIDlist AS CREATE LOCAL TEMPORARY TABLE IDlist (categoryID INT, minQuantity INT, maxQuantity INT) ON COMMIT DROP;
--- server/program/sqlite.sql
+++
@@ -1,9 +0,0 @@
-PREPARE getLastPictureID AS SELECT DISTINCT last_insert_rowid( ) AS "id" FROM Picture;
-PREPARE getLastConfigurationID AS SELECT DISTINCT last_insert_rowid( ) AS "id" FROM Configuration AS id;
-PREPARE createIdList AS CREATE TEMPORARY TABLE IF NOT EXISTS IDlist (ID INT);
-PREPARE createIdUnion AS CREATE TEMPORARY TABLE IF NOT EXISTS IDunion (ID INT);
-PREPARE dropIdList AS DROP TABLE IDList;
-PREPARE dropIdUnion AS DROP TABLE IDUnion;
-PREPARE createFeatureIDlist AS CREATE TEMPORARY TABLE IF NOT EXISTS IDlist (featureID INT, minQuantity INT, maxQuantity INT);
-PREPARE createComponentIDlist AS CREATE TEMPORARY TABLE IF NOT EXISTS IDlist (componentID INT, quantity INT);
-PREPARE createCategoryIDlist AS CREATE TEMPORARY TABLE IF NOT EXISTS IDlist (categoryID INT, minQuantity INT, maxQuantity INT);
